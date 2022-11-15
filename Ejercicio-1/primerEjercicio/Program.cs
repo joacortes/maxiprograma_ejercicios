3fbe519d93d22a40cb2d7508969cda76019d9851
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-15 19:13:03 -0300

    Subo el primer ejercicio

---
+++ Ejercicio-1/primerEjercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// 1. Para este primer ejercicio deberás crear tu primer programa que emita en pantalla "Hola Mundo".
+//Adicionalmente, podés hacer las modificaciones correspondientes para que el programa emita "Bienvenido Juanito", donde "Juanito" puede ser tu nombre. 
+
+namespace primerEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Joaquin");
+        }
+    }
+}
