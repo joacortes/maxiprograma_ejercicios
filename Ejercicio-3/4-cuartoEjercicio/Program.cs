f8be79417fdcb33a1cdac87b2859790598d5cc7f
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-15 22:11:25 -0300

    Subo el cuarto ejercicio de la Unidad 3

---
+++ Ejercicio-3/4-cuartoEjercicio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _4_cuartoEjercicio
+{
+    // 4. Hacer un programa para ingresar cuatro números distintos y luego mostrar por pantalla el menor de ellos.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, menor = 0;
+
+            Console.WriteLine("Ingrese el número 1: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 3: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 4: ");
+            n4 = int.Parse(Console.ReadLine());
+
+            if(n1 < n2 && n1 < n3 && n1 < n4){
+                menor = n1;
+            }else if (n2 < n1 && n2 < n3 && n2 < n4){
+                menor = n2;
+            }else if (n3 < n1 && n3 < n2 && n3 < n4){
+                menor = n3;
+            }else{
+                menor = n4;
+            }
+
+            Console.WriteLine("El menor es: {0}", menor);
+        }
+    }
+}
