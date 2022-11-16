8cb8e071d0aa445eddb464cbe33124d740464418
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-15 21:25:39 -0300

    Agrego los ejercicios de la unidad 3

---
+++ Ejercicio-3/1-primerEjercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1_primerEjercicio
+{
+    // 1. Hacer un programa para ingresar un número y luego se emita por pantalla un cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero > 10)
+                Console.WriteLine("El número es mayor a 10");
+            else
+                Console.WriteLine("El número no es mayor a 10");
+            
+        }
+    }
+}
