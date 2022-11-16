789e7118388558388a9da9d139c5617eaa6cb468
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-15 21:33:52 -0300

    Subo el segundo ejercicio de la Unidad 3

---
+++ Ejercicio-3/2-segundoEjercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2_segundoEjercicio
+{
+    class Program
+    {
+        // 2. Hacer un programa para ingresar un número y luego se emita un cartel por pantalla “Positivo” si el número es mayor a cero, “Negativo” si el número es menor a cero o “Cero” si el número es igual a cero.
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero > 0)
+                Console.WriteLine("Positivo");
+            else if(numero < 0)
+                Console.WriteLine("Negativo");
+            else
+                Console.WriteLine("Cero");
+        }
+    }
+}
