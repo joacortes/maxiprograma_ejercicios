50c20275b18f3f3926c3ad868b3f2e4373d14d6f
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-16 21:58:33 -0300

    actualizo los ejercicios de maxiprograma

---
+++ Ejercicio-4/1-primerEjercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1_primerEjercicio
+{
+    class Program
+    {
+// 1. Hacer un programa que solicite el ingreso de dos números y luego calcular:
+// La resta si el primero es mayor que el segundo.
+// La suma si son iguales.
+// El producto si el primero es menor.
+// Se deberá emitir un cartel por pantalla con el resultado correspondiente.
+        static void Main(string[] args)
+        {
+            int num, num2, output;
+
+            Console.WriteLine("Ingrese un número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if(num > num2)
+                output = num - num2;
+            else if(num < num2)
+                output = num * num2;
+            else
+                output = num + num2;
+
+            Console.WriteLine(output);
+        }
+    }
+}
