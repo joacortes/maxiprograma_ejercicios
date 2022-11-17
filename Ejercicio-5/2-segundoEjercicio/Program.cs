1e8947e849bc3fdcf7137111959d935c90c74706
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-17 01:25:11 -0300

    actualizo los ejercicios del curso de maxiprograma

---
+++ Ejercicio-5/2-segundoEjercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2_segundoEjercicio
+{
+    class Program
+    {
+        // 2. Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+        static void Main(string[] args)
+        {
+            int numero;
+            int mayor = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if(numero > mayor){
+                    mayor = numero;
+                }
+            }
+
+            Console.WriteLine("El mayor numero es: {0}", mayor);
+        }
+    }
+}
---
+++ Ejercicio-5/3-tercerEjercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_tercerEjercicio
+{
+    class Program
+    {
+        // 3. Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años.
+        static void Main(string[] args)
+        {
+            double edad, promedio, contador = 0, acumulador = 0; 
+            for(int i = 0; i < 20; i++){
+                Console.Write("Ingrese una edad: ");
+                edad = int.Parse(Console.ReadLine());
+
+                if(edad > 18){
+                    contador++;
+                    acumulador += edad;
+                }
+
+            }
+            promedio = acumulador / contador;
+            Console.WriteLine("{0} / {1}: {2}", acumulador, contador, promedio.ToString("00.00"));
+        }
+    }
+}
