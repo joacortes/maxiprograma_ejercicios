df7d12acf7474a74d09e782622aa14b7a8d9846a
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-18 02:02:35 -0300

    actualizo los ejercicios del curso de maxiprograma

---
+++ Ejercicio-5/4-cuartoEjercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _4_cuartoEjercicio
+{
+    class Program
+    {
+        // 4. Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.  Nota: un numero es primo cuando es divisible únicamente por 1 y por sí mismo.
+
+        // Si n / 1 da resto 0 y n / n da resto 0 y n / i except 
+        static void Main(string[] args)
+        {
+            int n, cont = 0;
+
+            Console.WriteLine("Ingrese un número: ");
+            n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i < n; i++){
+                if(n % i == 0)
+                    cont++;
+            }
+
+            if(cont >= 2)
+                Console.WriteLine("El número {0} no es primo", n);
+            else
+                Console.WriteLine("El número {0} es primo", n);
+
+            
+        }
+    }
+}
---
+++ Ejercicio-5/5-quintoEjercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5_quintoEjercicio
+{
+    class Program
+    {
+        // Hacer un programa que solicite 20 números y luego emitir por pantalla el máximo de los números pares y el mínimo de los números impares.
+        static void Main(string[] args)
+        {
+            int n, mayorPares = 0, mayorImpares = 0; 
+
+            for(int i = 0; i < 20; i++){
+                Console.WriteLine("Ingrese un número: ");
+                n = int.Parse(Console.ReadLine());
+                
+                if(n % 2 == 0){
+                    if(n > mayorPares){
+                        mayorPares = n;
+                    }
+                }else{
+                    if(n > mayorImpares){
+                        mayorImpares = n;
+                    }
+                }
+            }
+
+            Console.WriteLine("El número par mayor es: {0}\nEl número impar mayor es: {1}", mayorPares, mayorImpares);
+        }
+    }
+}
