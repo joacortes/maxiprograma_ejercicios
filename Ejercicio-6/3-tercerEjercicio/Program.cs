f7c9092ba57799fe4c6e0df2d3d9ee5a50d572a5
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2022-11-20 23:11:58 -0300

    actualizo los ejercicios del curso de maxiprograma

---
+++ Ejercicio-6/3-tercerEjercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3_tercerEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Hacer un programa para recibir listas de números positivos que están separadas entre sí por un cero. El fin de la carga se notifica con un número negativo. Luego mostrar cuántos números tiene cada lista.
+            
+            int numero = 1, contListas;
+
+            
+            while(numero >= 0){
+                contListas = 0;
+                Console.Write("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+                while(numero > 0){
+                    contListas++;
+
+                    Console.Write("Ingrese un número: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("En la lista se ingrearon un total de {0} numeros", contListas);
+            }
+        }
+    }
+}
